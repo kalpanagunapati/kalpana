e27bbd9ab60dcfa4c8aedb9c040c2e3243c4f6ff
kalpanagunapati <164157561+kalpanagunapati@users.noreply.github.com>
2025-01-29 20:53:02 +0530

    Create electronics.js

---
+++ electronics.js
@@ -0,0 +1,14 @@
+async function electronicsPriceAbove500 () {
+    try {
+        const products = await Product.find({ 
+            category: "Electronics", 
+            price: { $gt: 500 } 
+        }).sort({ price: -1 });
+
+        console.log(products);
+    } catch (error) {
+        console.error("Error fetching products:", error);
+    }
+}
+
+electronicsPriceAbove500();
